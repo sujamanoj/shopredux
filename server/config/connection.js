2ebb7fd1a9218bd78efa8110a59bb0c3ae76d11c
sujamanoj <suja@masstv.app>
2022-08-09 11:19:59 -0700

    heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mernshopping', {
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost:27017/shop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
